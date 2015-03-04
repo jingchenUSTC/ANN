52199b3afd836c0ea2b28300c3bc68067afb4ef2
chenjing <chenjing@chenjing-PC>
2015-03-04 18:59:59 +0800

    update

--- src/com/jingchen/util/DataUtil.java
+++ src/com/jingchen/util/DataUtil.java
@@ -16,59 +16,84 @@ public class DataUtil
 	private static DataUtil instance = null;
 	private Map<String, Integer> mTypes;
 	private int mTypeCount;
+
 	private DataUtil()
 	{
 		mTypes = new HashMap<String, Integer>();
 		mTypeCount = 0;
 	}
+
 	public static synchronized DataUtil getInstance()
 	{
-		if(instance == null)
+		if (instance == null)
 			instance = new DataUtil();
 		return instance;
-		
+
 	}
+
 	public Map<String, Integer> getTypeMap()
 	{
 		return mTypes;
 	}
-	public int getTypeCount(){
+
+	public int getTypeCount()
+	{
 		return mTypeCount;
 	}
-	public String getTypeName(int type){
-		if(type == -1)
+
+	public String getTypeName(int type)
+	{
+		if (type == -1)
 			return new String("无法判断");
 		Iterator<String> keys = mTypes.keySet().iterator();
-		while(keys.hasNext()){
+		while (keys.hasNext())
+		{
 			String key = keys.next();
-			if(mTypes.get(key) == type)
+			if (mTypes.get(key) == type)
 				return key;
 		}
 		return null;
 	}
+
 	/**
-	 * 根据文件生成训练集
+	 * 根据文件生成训练集，注意：程序将以第一个出现的非数字的属性作为类别名称
+	 * 
 	 * @param fileName
+	 *            文件名
+	 * @param sep
+	 *            分隔符
 	 * @return
 	 * @throws Exception
 	 */
-	public List<DataNode> getDataList(String fileName, String sep) throws Exception{
+	public List<DataNode> getDataList(String fileName, String sep)
+			throws Exception
+	{
 		List<DataNode> list = new ArrayList<DataNode>();
-		BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+		BufferedReader br = new BufferedReader(new FileReader(
+				new File(fileName)));
 		String line = null;
-		while((line = br.readLine()) != null){
+		while ((line = br.readLine()) != null)
+		{
 			String splits[] = line.split(sep);
 			DataNode node = new DataNode();
 			int i = 0;
-			for(; i < splits.length-1; i++)
-				node.addAttrib(Float.valueOf(splits[i]));
-			if(!mTypes.containsKey(splits[i]))
+			for (; i < splits.length; i++)
 			{
-				mTypes.put(splits[i], mTypeCount);
-				mTypeCount++;
+				try
+				{
+					node.addAttrib(Float.valueOf(splits[i]));
+				} catch (Exception e)
+				{
+					// 非数字，则为类别名称，将类别映射为数字
+					if (!mTypes.containsKey(splits[i]))
+					{
+						mTypes.put(splits[i], mTypeCount);
+						mTypeCount++;
+					}
+					node.setType(mTypes.get(splits[i]));
+					list.add(node);
+				}
 			}
-			node.setType(mTypes.get(splits[i]));
-			list.add(node);
 		}
 		return list;
 	}
